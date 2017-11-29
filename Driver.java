d3ef4b6394cd8cf9f1d5c0226b61372f46e7a0e9
Rachel Kelley <rachakelley@gmail.com>
2017-11-29 17:11:09 -0500

    indentation fixes

--- Driver.java
+++ Driver.java
@@ -6,17 +6,17 @@ public class Driver {
 			Scanner keyIn = new Scanner(System.in);
 			System.out.println("Enter the size n (number of nodes) "
 					+ "of the adjacency matrix:");
-		    int n = keyIn.nextInt();
-		    System.out.println("Enter the nxn adjacency matrix:");
-	        int[][] graph = new int[n][n];
-	        for (int i = 0; i < n; i++)
-	            for (int j = 0; j < n; j++)
-	            	graph[i][j] = keyIn.nextInt();
-	        System.out.println("You entered the adjacency matrix:");
-	        display(graph);
+			int n = keyIn.nextInt();
+			System.out.println("Enter the nxn adjacency matrix:");
+			int[][] graph = new int[n][n];
+			for (int i = 0; i < n; i++)
+			    for (int j = 0; j < n; j++)
+				graph[i][j] = keyIn.nextInt();
+			System.out.println("You entered the adjacency matrix:");
+			display(graph);
 	        
 			// showing the vertex set of the graph
-	        System.out.print("Vertex set:\n{");
+	      		System.out.print("Vertex set:\n{");
 			for(int row = 0; row < graph.length; row++)
 				for(int col = 0; col < graph.length; col++)
 					if(graph[row][col] == 1) {
@@ -35,8 +35,8 @@ public class Driver {
 					System.out.println();
 				}
 			}
-		    else
-		    	System.out.println("Graph is not bipartite");
+		    	else
+		    		System.out.println("Graph is not bipartite");
 		} catch(InputMismatchException e) {
 			System.out.println("Error: Invalid input.");
 		}
@@ -49,31 +49,31 @@ public class Driver {
 		int colors[] = new int[vertices];
 		// initializing all vertices(indices of array) with no color
 		for (int i = 0; i < vertices; i++)
-            colors[i] = -1;	
+            		colors[i] = -1;	
 		// choosing a source vertex and assigning it the first color (red).
-        colors[source] = 1;
+       		colors[source] = 1;
         
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        // adding the first colored vertex to the queue
-        queue.add(source);
-        while (queue.size() > 0) {
-        	// Remove vertex from the queue and assign it to 'i'
-            int i = queue.poll();
-            // looking at all of the other adjacent vertices that have
-            // not yet been colored yet
-            for (int vertex = 0; vertex < vertices; vertex++) {
-                // If there is an edge between i and vertex and vertex has not
-                // been colored, assign the other color to the vertex adjacent
-            	// to 'i'.
-                if (graph[i][vertex] == 1 && colors[vertex] == -1) {
-                    colors[vertex] = 1 - colors[i];
-                    queue.add(vertex);
-                }
-                // is not bipartite. (there is an edge from i to vertex,
-                // and vertex and 'i' already have the same color assigned to them)
-                else if (graph[i][vertex] == 1 && colors[vertex] == colors[i])
-                    return false;
-            }
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		// adding the first colored vertex to the queue
+		queue.add(source);
+		while (queue.size() > 0) {
+			// Remove vertex from the queue and assign it to 'i'
+		    int i = queue.poll();
+		    // looking at all of the other adjacent vertices that have
+		    // not yet been colored yet
+		    for (int vertex = 0; vertex < vertices; vertex++) {
+			// If there is an edge between i and vertex and vertex has not
+			// been colored, assign the other color to the vertex adjacent
+			// to 'i'.
+			if (graph[i][vertex] == 1 && colors[vertex] == -1) {
+			    colors[vertex] = 1 - colors[i];
+			    queue.add(vertex);
+			}
+			// is not bipartite. (there is an edge from i to vertex,
+			// and vertex and 'i' already have the same color assigned to them)
+			else if (graph[i][vertex] == 1 && colors[vertex] == colors[i])
+			    return false;
+		    }
         }
         
         StringBuilder sb = new StringBuilder();
@@ -89,7 +89,7 @@ public class Driver {
         sb.delete(sb.lastIndexOf(", "), sb.length());
         sb2.delete(sb2.lastIndexOf(", "), sb2.length());
         System.out.print("Red vertices: " + sb.toString() +
-        		("\nBlue vertices: " + sb2.toString()) + "\n");
+        ("\nBlue vertices: " + sb2.toString()) + "\n");
         // is bipartite
         return true;
     }
@@ -97,19 +97,19 @@ public class Driver {
 	public static void display(int[][] graph) {
 		int n = graph.length;
 		System.out.print(" ");
-	    for (int col = 0; col < n; col++)
-	    	System.out.print("  " + col);
-	    System.out.println();
-	    for (int col = 0; col < n; col++) {
-	        System.out.print(col + " ");
-	        for (int row = 0; row < n; row++) {
-	        	if (graph[col][row] != 0) 
-	        		System.out.print(" 1 ");
-	            else                  
-	                System.out.print(" 0 ");
-	        }
-	        System.out.println();
-	    }
-	    System.out.println();
+	    	for (int col = 0; col < n; col++)
+	    		System.out.print("  " + col);
+	    	System.out.println();
+		for (int col = 0; col < n; col++) {
+			System.out.print(col + " ");
+			for (int row = 0; row < n; row++) {
+				if (graph[col][row] != 0) 
+					System.out.print(" 1 ");
+				else                  
+					System.out.print(" 0 ");
+			}
+			System.out.println();
+		}
+	    	System.out.println();
 	}
 }
